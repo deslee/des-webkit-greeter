6cd200092e5bd7189050daf19eb58db2230c5535
Desmond Lee <desmondclee@gmail.com>
2014-09-14 01:48:37 -0500

    test in vm

--- build/app.js
+++ build/app.js
@@ -46,43 +46,6 @@ function authentication_complete() {
 
 'app controller goes here';
 'common service goes here';
-'use strict';
-
-angular.module('des-webkit-greeter-main', ['ngRoute', 'user'])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/login/', {
-                templateUrl: 'login/login.html',
-                controller: 'LoginCtrl'
-            });
-    })
-    .controller('LoginCtrl', function ($rootScope, $scope, $routeParams) {
-        $scope.user = $routeParams;
-        $scope.login = function (form) {
-            NProgress.start();
-            NProgress.set(0.4);
-            lightdm.provide_secret(form.password);
-        };
-        $scope.$on('authentication_complete', function () {
-            NProgress.done();
-            if (lightdm.is_authenticated) {
-                lightdm.login(lightdm.authentication_user, lightdm.default_session);
-            }
-            else {
-                lightdm.start_authentication($scope.user.name);
-                $scope.form.password = '';
-            }
-        });
-        console.log($rootScope.authenticating);
-        if ($rootScope.authenticating) {
-            lightdm.cancel_authentication();
-        }
-        $rootScope.authenticating = true;
-        lightdm.start_authentication($scope.user.name);
-        $('.pwinput').focus();
-    });
-
-
 /**
  * Created by desmond on 9/13/2014.
  */
@@ -118,6 +81,40 @@ angular.module('pickUser', [])
             }
         })
     });
+'use strict';
+
+angular.module('des-webkit-greeter-main', ['ngRoute', 'user'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/login/', {
+                templateUrl: 'login/login.html',
+                controller: 'LoginCtrl'
+            });
+    })
+    .controller('LoginCtrl', function ($rootScope, $scope, $routeParams) {
+        $scope.user = $routeParams;
+        $scope.login = function (form) {
+            lightdm.provide_secret(form.password);
+        };
+        $scope.$on('authentication_complete', function () {
+            if (lightdm.is_authenticated) {
+                lightdm.login(lightdm.authentication_user, lightdm.default_session);
+            }
+            else {
+                lightdm.start_authentication($scope.user.name);
+                $scope.form.password = '';
+            }
+        });
+        console.log($rootScope.authenticating);
+        if ($rootScope.authenticating) {
+            lightdm.cancel_authentication();
+        }
+        $rootScope.authenticating = true;
+        lightdm.start_authentication($scope.user.name);
+        $('.pwinput').focus();
+    });
+
+
 /**
  * Created by desmond on 9/13/2014.
  */
